c0630dab3b270ec5e0d2ba80e09a90f5202e9e22
yoownny <kelosy00@naver.com>
2024-11-09 22:19:38 +0900

    feat(Receipt): Implement Receipt
    - Receipt 기본틀 작성

--- src/main/java/store/domain/receipt/Receipt.java
+++ src/main/java/store/domain/receipt/Receipt.java
@@ -1,4 +1,18 @@
 package store.domain.receipt;
 
+import java.util.List;
+
 public class Receipt {
+    private static final double MEMBERSHIP_DISCOUNT_RATE = 0.3;
+    private static final int MAX_MEMBERSHIP_DISCOUNT = 8000;
+
+    private final List<ReceiptItem> items;
+    private final int promotionDiscount;
+    private final boolean useMembership;
+
+    public Receipt(final List<ReceiptItem> items, final int promotionDiscount, final boolean useMembership) {
+        this.items = items;
+        this.promotionDiscount = promotionDiscount;
+        this.useMembership = useMembership;
+    }
 }

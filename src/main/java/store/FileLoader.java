66b3cb9788c9311ada44f74e65652a58b9a94c43
yoownny <kelosy00@naver.com>
2024-11-09 22:31:00 +0900

    feat(InputView): Update InputView
    - 프로모션 관련 질문 추가

---
+++ src/main/java/store/FileLoader.java
@@ -0,0 +1,8 @@
+package store;
+
+public class FileLoader {
+    private static final String PRODUCTS_FILE = "src/main/resources/products.md";
+    private static final String PROMOTIONS_FILE = "src/main/resources/promotions.md";
+
+
+}
--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -83,6 +83,24 @@ public class InputView {
         }
     }
 
+
+    public boolean readAdditionalPurchase(String productName, int plusQuantity) {
+        System.out.printf("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n",
+                productName, plusQuantity);
+        String input = Console.readLine();
+        validateYesNo(input);
+        return input.trim().toUpperCase().equals("Y");
+    }
+
+    public boolean readPromotionPurchase(String productName, int exceptQuantity) {
+        System.out.printf("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n",
+                productName, exceptQuantity);
+        String input = Console.readLine();
+        validateYesNo(input);
+        return input.trim().toUpperCase().equals("Y");
+    }
+
+
     public boolean readMembershipOption() {
         while (true) {
             try {

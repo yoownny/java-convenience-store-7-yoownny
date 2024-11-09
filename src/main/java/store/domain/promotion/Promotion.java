856aa322aab88f6c7b5bf09485c2f54dd887e731
yoownny <kelosy00@naver.com>
2024-11-09 22:15:27 +0900

    feat(Promotion): Implement Promotion
    - Promotion 기본틀 작성

--- src/main/java/store/domain/promotion/Promotion.java
+++ src/main/java/store/domain/promotion/Promotion.java
@@ -1,4 +1,19 @@
 package store.domain.promotion;
 
+import java.time.LocalDate;
+
 public class Promotion {
+    private final String name;
+    private final int buyQuantity;
+    private final int giftQuantity;
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+
+    public Promotion(String name, int buyQuantity, int giftQuantity, LocalDate startDate, LocalDate endDate) {
+        this.name = name;
+        this.buyQuantity = buyQuantity;
+        this.giftQuantity = giftQuantity;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
 }

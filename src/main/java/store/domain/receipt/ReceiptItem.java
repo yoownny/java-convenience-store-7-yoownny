d5bee5a6effcfa005cc7eb499e26368eec2555f2
yoownny <kelosy00@naver.com>
2024-11-09 22:17:59 +0900

    feat(ReciptItem): Implement ReciptItem
    - ReciptItem 기본틀 작성

--- src/main/java/store/domain/receipt/ReceiptItem.java
+++ src/main/java/store/domain/receipt/ReceiptItem.java
@@ -1,4 +1,15 @@
 package store.domain.receipt;
 
 public class ReceiptItem {
+    private final String name;
+    private final int quantity;
+    private final int price;
+    private final int giftQuantity;
+
+    public ReceiptItem(String name, int quantity, int price, int giftQuantity) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+        this.giftQuantity = giftQuantity;
+    }
 }
